e8b998776d4a893779ff2ca74dd3c5e967a55c2d
a Comfortable Cow <jimwei2022@u.northwestern.edu>
2023-01-04 09:58:16 -0600

    Attempting 25 box generation

--- generate.py
+++ generate.py
@@ -8,6 +8,14 @@ y = 0
 z = 0.5
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[1,1,1])
 
-pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box2", pos=[x +0.5 ,y,z+1] , size=[1,1,1])
+
+for i in range(5):
+    for o in range (5):
+        for p in range (5):
+            pyrosim.Send_Cube(name="25Box", pos=[x + i*2,y + o*2,z + p*2] , size=[1,1,1])
+
+    
+
 
 pyrosim.End()
