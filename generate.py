117417683d4916e469274a0a3fd9020b67366d92
a Comfortable Cow <jimwei2022@u.northwestern.edu>
2023-01-04 10:42:52 -0600

    CreateWorld()

--- generate.py
+++ generate.py
@@ -1,13 +1,11 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
+
 
 #length, width, height
 x = 0
 y = 0
 z = 0
-
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[1,1,1])
 
 #pyrosim.Send_Cube(name="Box2", pos=[x +0.5 ,y,z+1] , size=[1,1,1])
 
@@ -19,5 +17,11 @@ for i in range(5):
             pyrosim.Send_Cube(name="25Box", pos=[x + i*2,y + o*2,z + p*2] , size=[1,1,1])
 """
 
+#E. Joints
 
-pyrosim.End()
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[1,1,1])
+    pyrosim.End()
+
