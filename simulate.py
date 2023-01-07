fec6e05f26656b8b0e7fb1d72d32ad4e0ca24e81
a Comfortable Cow <jimwei2022@u.northwestern.edu>
2023-01-06 18:03:39 -0600

    numpy, and sensor save

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy as numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -20,12 +21,21 @@ robotID = p.loadURDF("body.urdf")
 #Simulation setup
 pyrosim.Prepare_To_Simulate(robotID)
 
-for i in range(1000):
+#numpy vector to save sensor values, same iterations as for loop
+backLegSensorValues = numpy.zeros(100)
+
+
+for i in range(100):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    #print(backLegTouch)
 
     time.sleep(1/600)
     #print(i)
+
+print("BACKLEG SENSOR VALUES")
+print(backLegSensorValues)
+numpy.save('/Users/jim/Documents/GitHub/CS-396-Artificial-Life-Bots/data/test.npy', backLegSensorValues)
+
 
 p.disconnect()
