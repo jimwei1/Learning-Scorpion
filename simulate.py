0a3e3a43c338565b46f35dee5a691693b3beb2d9
a Comfortable Cow <jimwei2022@u.northwestern.edu>
2023-01-06 18:43:17 -0600

    cleaned up files

--- simulate.py
+++ simulate.py
@@ -31,7 +31,7 @@ for i in range(100):
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
-    pyrosim.Set_Motor_For_Joint(bodyIndex = ..., jointName = "...", controlMode = ..., targetPosition = ..., maxForce = ...)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = "Torso_BackLeg", controlMode = ..., targetPosition = ..., maxForce = ...)
 
     time.sleep(1/600)
 
