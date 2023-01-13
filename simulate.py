2e81b4b05bfb83169481b8498463bbcef158b22a
a Comfortable Cow <jimwei2022@u.northwestern.edu>
2023-01-13 16:11:09 -0600

    G step 23

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as numpy
+import random as random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -21,18 +22,32 @@ robotID = p.loadURDF("body.urdf")
 #Simulation setup
 pyrosim.Prepare_To_Simulate(robotID)
 
+timevalue = 1000
+anchortimevalue = 1000
+
 #numpy vector to save sensor values, same iterations as for loop
-backLegSensorValues = numpy.zeros(1000)
-frontLegSensorValues = numpy.zeros(1000)
+backLegSensorValues = numpy.zeros(timevalue)
+frontLegSensorValues = numpy.zeros(timevalue)
 
 
-for i in range(1000):
+for i in range(timevalue):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotID, jointName = b'Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotID, jointName = b'Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
+
+    #pyrosim.Set_Motor_For_Joint(bodyIndex = robotID, jointName = b'Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = -1, maxForce = 500)
+    #pyrosim.Set_Motor_For_Joint(bodyIndex = robotID, jointName = b'Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = 1, maxForce = 500)
+
+        #random
+    #pyrosim.Set_Motor_For_Joint(bodyIndex = robotID, jointName = b'Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-1.57, 1.57), maxForce = 50)
+    #pyrosim.Set_Motor_For_Joint(bodyIndex = robotID, jointName = b'Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-1.57, 1.57), maxForce = 50)
+
+        #sin
+    targetAngles = numpy.sin((6.28 * anchortimevalue) / timevalue)
+
+    #pyrosim.Set_Motor_For_Joint(bodyIndex = robotID, jointName = b'Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = numpy.sin(targetAngles), maxForce = 50)
+    #pyrosim.Set_Motor_For_Joint(bodyIndex = robotID, jointName = b'Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = numpy.sin(targetAngles), maxForce = 50)
 
     time.sleep(1/600)
 
