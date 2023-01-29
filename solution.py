9e372852afaa805157b179752000b338eae88252
a Comfortable Cow <jimwei2022@u.northwestern.edu>
2023-01-29 17:01:14 -0600

    dunno

--- solution.py
+++ solution.py
@@ -48,12 +48,12 @@ class SOLUTION:
         pyrosim.Send_Motor_Neuron(name = 3 , jointName = "Torso_BackLeg")
         pyrosim.Send_Motor_Neuron(name = 4 , jointName = "Torso_FrontLeg")
 
-        #pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1)
-        #pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1)
-        #pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 4 , weight = 1)
-        #pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 4 , weight = 1)
-        #pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 4 , weight = 1)
-        #pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 3 , weight = 1)
+        pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1)
+        pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1)
+        pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 4 , weight = 1)
+        pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 4 , weight = 1)
+        pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 4 , weight = 1)
+        pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 3 , weight = 1)
         
         for currentRow in [0,1,2]:
             for currentColumn in [0,1]:
